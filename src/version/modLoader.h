59953610f362f6b2abf13508d4dd741b9aefdda4
Glumboi <58010838+Glumboi@users.noreply.github.com>
2024-02-11 00:49:18 +0100

    upload

--- src/version/modLoader.h
+++ src/version/modLoader.h
@@ -15,31 +15,40 @@
 #define KEYISNOTPRESSED > 0
 
 enum ModLoaderInitState
-{ //--> implement at some point
-	UNDEFINED,
-	SUCC,
-	ERR,
-	WARN
+{
+    //--> implement at some point
+    UNDEFINED,
+    SUCC,
+    ERR,
+    WARN
 };
 
 class ModLoader
 {
 public:
-	bool skipASI = true;
-	static HANDLE gameHandle;
-	ModLoader();
-	void Init();
-	void LoadConfig(const std::string fileName);
-	void LoadAllPlugins();
-	void UnloadAllPlugins();
+    bool skipASI = true;
+    static HANDLE gameHandle;
+    ModLoader();
+    void Init();
+    void LoadConfig(const std::string fileName);
+    void LoadAllPlugins();
+    void UnloadAllPlugins();
+    void DumpIL2CPPBinary();
 
 private:
-	std::string pluginsPath;
-	std::vector<HMODULE> loadedPlugins;
-	void LoadPlugin(const std::string& path);
+    std::string pluginsPath;
+    std::string appPath;
+    std::string appDataPath;
+    std::string gameAsmPath;
+    std::string gameMetadataPath;
+    std::string dumperPath;
+    std::string command;
+    std::string dumpOutPath;
+    std::vector<HMODULE> loadedPlugins;
+    void LoadPlugin(const std::string& path);
 };
 
 struct KeyboardHandler
 {
-	static void KeyBoardLoop(ModLoader* loaderInstance);
+    static void KeyBoardLoop(ModLoader* loaderInstance);
 };
